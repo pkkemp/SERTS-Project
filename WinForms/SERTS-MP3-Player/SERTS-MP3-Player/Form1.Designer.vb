934d3b250c742625d26b9f8354d5779cdecaf933
Sriver <tyler.w.sriver@eagles.oc.edu>
2016-11-07 09:30:56 -0600

    UI Modifications

--- WinForms/SERTS-MP3-Player/SERTS-MP3-Player/Form1.Designer.vb
+++ WinForms/SERTS-MP3-Player/SERTS-MP3-Player/Form1.Designer.vb
@@ -24,18 +24,20 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.playBtn = New System.Windows.Forms.Button()
+        Me.songProgressBar = New System.Windows.Forms.ProgressBar()
+        Me.backBtn = New System.Windows.Forms.Button()
+        Me.nextBtn = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
         Me.Label1.Location = New System.Drawing.Point(13, 13)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(66, 13)
+        Me.Label1.Size = New System.Drawing.Size(76, 13)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Track Name"
         '
@@ -43,58 +45,68 @@ Partial Class Form1
         '
         Me.Label2.AccessibleName = "currentTrack"
         Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
         Me.Label2.Location = New System.Drawing.Point(39, 38)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(83, 13)
+        Me.Label2.Size = New System.Drawing.Size(95, 13)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "[TRACK NAME]"
         '
-        'Button1
+        'playBtn
         '
-        Me.Button1.AccessibleName = "playBtn"
-        Me.Button1.Location = New System.Drawing.Point(254, 179)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 2
-        Me.Button1.Text = "Play"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.playBtn.AccessibleName = "playBtn"
+        Me.playBtn.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom), System.Windows.Forms.AnchorStyles)
+        Me.playBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.playBtn.Location = New System.Drawing.Point(129, 93)
+        Me.playBtn.Name = "playBtn"
+        Me.playBtn.Size = New System.Drawing.Size(75, 23)
+        Me.playBtn.TabIndex = 2
+        Me.playBtn.Text = "Play"
+        Me.playBtn.UseVisualStyleBackColor = True
         '
-        'ProgressBar1
+        'songProgressBar
         '
-        Me.ProgressBar1.Location = New System.Drawing.Point(16, 157)
-        Me.ProgressBar1.Name = "ProgressBar1"
-        Me.ProgressBar1.Size = New System.Drawing.Size(557, 11)
-        Me.ProgressBar1.TabIndex = 3
+        Me.songProgressBar.ForeColor = System.Drawing.Color.Lime
+        Me.songProgressBar.Location = New System.Drawing.Point(12, 76)
+        Me.songProgressBar.Name = "songProgressBar"
+        Me.songProgressBar.Size = New System.Drawing.Size(317, 11)
+        Me.songProgressBar.TabIndex = 3
         '
-        'Button3
+        'backBtn
         '
-        Me.Button3.AccessibleName = "backBtn"
-        Me.Button3.Location = New System.Drawing.Point(137, 179)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 23)
-        Me.Button3.TabIndex = 5
-        Me.Button3.Text = "Back"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.backBtn.AccessibleName = "backBtn"
+        Me.backBtn.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom), System.Windows.Forms.AnchorStyles)
+        Me.backBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.backBtn.Location = New System.Drawing.Point(85, 93)
+        Me.backBtn.Name = "backBtn"
+        Me.backBtn.Size = New System.Drawing.Size(38, 23)
+        Me.backBtn.TabIndex = 5
+        Me.backBtn.Text = "<"
+        Me.backBtn.UseVisualStyleBackColor = True
         '
-        'Button2
+        'nextBtn
         '
-        Me.Button2.AccessibleName = "forwardBtn"
-        Me.Button2.Location = New System.Drawing.Point(371, 179)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 4
-        Me.Button2.Text = "Forward"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.nextBtn.AccessibleName = "forwardBtn"
+        Me.nextBtn.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom), System.Windows.Forms.AnchorStyles)
+        Me.nextBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nextBtn.Location = New System.Drawing.Point(210, 93)
+        Me.nextBtn.Name = "nextBtn"
+        Me.nextBtn.Size = New System.Drawing.Size(38, 23)
+        Me.nextBtn.TabIndex = 4
+        Me.nextBtn.Text = ">"
+        Me.nextBtn.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(585, 256)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.ProgressBar1)
-        Me.Controls.Add(Me.Button1)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(341, 136)
+        Me.Controls.Add(Me.backBtn)
+        Me.Controls.Add(Me.nextBtn)
+        Me.Controls.Add(Me.songProgressBar)
+        Me.Controls.Add(Me.playBtn)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
@@ -106,8 +118,8 @@ Partial Class Form1
 
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
-    Friend WithEvents Button1 As Button
-    Friend WithEvents ProgressBar1 As ProgressBar
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button2 As Button
+    Friend WithEvents playBtn As Button
+    Friend WithEvents songProgressBar As ProgressBar
+    Friend WithEvents backBtn As Button
+    Friend WithEvents nextBtn As Button
 End Class
