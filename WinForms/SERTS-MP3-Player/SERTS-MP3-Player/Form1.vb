9c9a029e672d69afcf2254ad5fdf360ac38bf1e8
prestonkemp <preston@prestonkemp.com>
2016-11-29 08:39:53 -0600

    Changes to gitignore

--- WinForms/SERTS-MP3-Player/SERTS-MP3-Player/Form1.vb
+++ WinForms/SERTS-MP3-Player/SERTS-MP3-Player/Form1.vb
@@ -1,7 +1,12 @@
 ﻿Imports System.Threading
 
 
+
+
 Public Class Form1
+    ' This is a definition of a thread. The thread object is Thread_0 and
+    ' it uses the method Thread_0_method() below as its code.
+    Dim Thread_0 As New Thread(AddressOf Thread_0_method)
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
